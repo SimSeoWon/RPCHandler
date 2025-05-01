41228f63703c1acad186a1d63980adb3e58009aa
Simkung <janus6685@gmail.com>
2025-05-01 23:13:58 +0900

    불필요한 파일 제거.

--- Source/ServerTest/ServerTest.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ServerTest.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ServerTest, "ServerTest" );
-
-DEFINE_LOG_CATEGORY(LogServerTest)
- 
